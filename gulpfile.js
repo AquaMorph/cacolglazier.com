99dc70fe0cb060de9530a90b04885d9a3c77d7c6
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-12-31 10:49:39 -0500

    Removed minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
-var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 
 gulp.task('deploy', function() {
